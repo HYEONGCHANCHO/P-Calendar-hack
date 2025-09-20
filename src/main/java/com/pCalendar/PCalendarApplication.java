aa6b1c61862086a468f291ee19cc8f92e45a3cd9
CHOHYEONGCHAN <wjstktpghd2@naver.com>
2025-09-21 00:47:35 +0900

    #2 설정파일 생성

---
+++ src/main/java/com/pCalendar/PCalendarApplication.java
@@ -0,0 +1,11 @@
+package com.pCalendar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PCalendarApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PCalendarApplication.class, args);
+    }
+}
